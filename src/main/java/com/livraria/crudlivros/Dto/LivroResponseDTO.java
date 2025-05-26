f3935be257a56cc5fefb97de1a97c4f6e0116527
ExoticAndrew <andrewantunes61@gmail.com>
2025-05-26 04:11:12 -0300

    feat: add LivroResponseDTO

---
+++ src/main/java/com/livraria/crudlivros/Dto/LivroResponseDTO.java
@@ -0,0 +1,17 @@
+package com.livraria.crudlivros.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LivroResponseDTO {
+
+    private Long idLivro;
+    private String name;
+    private String editora;
+    private int anoPublicacao;
+    private Double preco;
+    private EscritorDTO autor;
+
+}
