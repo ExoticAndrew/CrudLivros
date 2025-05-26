18536f4d4f191326468a90fbece5e8c1dfa130d8
ExoticAndrew <andrewantunes61@gmail.com>
2025-05-26 13:12:18 -0300

    feat: add EscritorService

---
+++ src/main/java/com/livraria/crudlivros/Service/EscritorService.java
@@ -0,0 +1,38 @@
+package com.livraria.crudlivros.Service;
+
+import com.livraria.crudlivros.Model.Escritor;
+import com.livraria.crudlivros.Repository.EscritorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class EscritorService {
+    private final EscritorRepository escritorRepository;
+
+    public EscritorService(EscritorRepository escritorRepository){
+        this.escritorRepository = escritorRepository;
+    }
+    public List<Escritor> listarTodos(){
+        return escritorRepository.findAll();
+    }
+    public Escritor buscarPorId(Long id){
+        return escritorRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Escritor não encontrado"));
+    }
+    public Escritor salvar(Escritor escritor){
+        return escritorRepository.save(escritor);
+    }
+     public Escritor atualizar(Long id, Escritor escritorAtualizado){
+        Escritor existente = buscarPorId(id);
+        existente.setName(escritorAtualizado.getName());
+        existente.setCpf(escritorAtualizado.getCpf());
+        existente.setEmail(escritorAtualizado.getEmail());
+        existente.setIdade(escritorAtualizado.getIdade());
+        return escritorRepository.save(existente);
+     }
+
+
+
+}
