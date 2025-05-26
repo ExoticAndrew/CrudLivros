4a0c41f6b3a8d416582b48cc32229810d2ced2d1
ExoticAndrew <andrewantunes61@gmail.com>
2025-05-26 02:55:17 -0300

    feat: add Escritor model
    criando o modelo escritor

---
+++ src/main/java/com/livraria/crudlivros/Model/Escritor.java
@@ -0,0 +1,38 @@
+package com.livraria.crudlivros.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Escritor {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    private Long idAutor;
+
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 11, max = 11, message = "O cpf deve ter 11 digitos")
+    private String cpf;
+
+    @Email(message = "Email inválido" )
+    private String email;
+
+    @Min(value = 18, message = "A idade mínima é 18 anos")
+    private int idade;
+
+
+
+}
