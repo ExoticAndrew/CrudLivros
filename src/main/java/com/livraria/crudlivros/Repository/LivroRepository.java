dfe44ace871d9a9a899ce98179b34f1029be0c9f
ExoticAndrew <andrewantunes61@gmail.com>
2025-05-26 04:23:20 -0300

    feat: add LivroRepository

---
+++ src/main/java/com/livraria/crudlivros/Repository/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.livraria.crudlivros.Repository;
+
+import com.livraria.crudlivros.Model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+
+    List<Livro> findByAutorNomeContainingIgnoreCase(String nomeAutor);
+}
