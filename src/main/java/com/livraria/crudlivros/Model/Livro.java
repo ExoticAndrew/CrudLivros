e4a42f68bed62b8736add234e297559202732dbc
ExoticAndrew <andrewantunes61@gmail.com>
2025-05-27 16:54:19 -0300

    feat(model): add validations and timestamps to Livro entity

--- src/main/java/com/livraria/crudlivros/Model/Livro.java
+++ src/main/java/com/livraria/crudlivros/Model/Livro.java
@@ -6,13 +6,17 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
-
 public class Livro {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idLivro;
@@ -25,10 +29,19 @@ public class Livro {
     @NotNull(message = "O autor é obrigatório")
     private Escritor autor;
 
-    @NotNull(message ="O preço é obrigatório")
+    @NotNull(message = "O preço é obrigatório")
     private Double preco;
 
-    @NotBlank(message = "Editora é obrigatório")
+    @NotBlank(message = "A editora é obrigatória")
     private String editora;
-    private int anoPublicacao;
+
+    @NotNull(message = "O ano de publicação é obrigatório")
+    private Integer anoPublicacao;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime dataCriacao;
+
+    @UpdateTimestamp
+    private LocalDateTime dataAtualizacao;
 }
